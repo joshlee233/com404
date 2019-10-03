2032c3731192b4a0507fad45217326918f5ea63c
Josh Lee <joshlee12@virginmedia.com>
2019-10-03 08:28:24 +0000

    If statements

---
+++ 1-basics/3-decision/1-if/bot.py
@@ -0,0 +1,11 @@
+#Prints the string
+print("What type of book is this?")
+#Stores the input in a variable
+book_type = input()
+
+#Checks whether the string entered is equal to adventure, if true then it prints the next line
+if (book_type == "adventure"):
+    print("I like adventure books!")
+
+#prints the string
+print("finished reading book")
