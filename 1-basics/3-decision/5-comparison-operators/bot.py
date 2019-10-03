8ffeedf157af289bf05307cfea546c731fda95be
Josh Lee <joshlee12@virginmedia.com>
2019-10-03 09:25:21 +0000

    Whats bigger?

---
+++ 1-basics/3-decision/5-comparison-operators/bot.py
@@ -0,0 +1,8 @@
+first_number = input("Please enter the first number: ")
+second_number = input("Please enter the second number: ")
+if (first_number > second_number):
+    print("Second number is bigger")
+elif (second_number > first_number):
+    print("First number is bigger")
+else:
+    print("They are both equal!")
