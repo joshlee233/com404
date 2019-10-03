fb0f936fe16f1a52d4bbb737b87a6975d7a77e03
Josh Lee <joshlee12@virginmedia.com>
2019-10-03 09:13:12 +0000

    If Elif Else

---
+++ 1-basics/3-decision/3-if-elif-else/bot.py
@@ -0,0 +1,11 @@
+print("which direction should I paint? (up, down, left right)")
+direction = input()
+
+if (direction == "up"):
+    print("I am painting in the upward direction!")
+elif (direction == "down"):
+    print("I am painting in the downward direction!")
+elif (direction == "right"):
+    print("I am painting in the right direction!")
+else:
+    print("I am painting in the left direction!")
