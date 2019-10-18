59d7cf2e364ca430612182d9848d9b2b57ec1292
Josh Lee <joshlee12@virginmedia.com>
2019-10-18 09:49:24 +0000

    demonstrate the implementation of multiple user defined functions.

--- 1-basics/5-functions/6-multiple-functions/bot.py
+++ 1-basics/5-functions/6-multiple-functions/bot.py
@@ -1,10 +1,11 @@
 def display_ladder(steps):
     for step in range(steps):
         print("| |\n***")
+    print("| |")
 
-def create_ladder(steps):
+def create_ladder():
   print("How many steps remain?: ")
   steps = int(input())
   display_ladder(steps)
 
-create_ladder(0)
+create_ladder()
